9f9a3ef3ab60bfd7a550df80350263c91d1536db
kaiftech-stack <kaifpathan535@gmail.com>
2026-01-14 15:01:49 +0530

    Day 6: Spring Boot setup and basic test endpoint

---
+++ src/main/java/com/germs/germs_auth/GermsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.germs.germs_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermsAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/germs/germs_auth/GermsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germs.germs_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
